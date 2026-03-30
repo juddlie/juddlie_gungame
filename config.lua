8353a4a5c6cc44f18b14675008cf0195e1dc1f69
juddlie <271890573+juddlie@users.noreply.github.com>
2026-03-30 18:38:29 -0400

    feat(config): add notify function and weapons

--- config.lua
+++ config.lua
@@ -3,4 +3,20 @@ local config = {}
 ---@type "esx" | "ox"
 config.framework = "esx"
 
+config.weapons = {
+  [0] = { label = "AP Pistol", hash = `WEAPON_APPISTOL` },
+  [1] = { label = "Carbine Rifle", hash = `WEAPON_CARBINERIFLE` }
+}
+
+---@param message string
+---@param type "success" | "error" | "info"
+---@param source number?
+config.notify = function(message, type, source)
+  if IsDuplicityVersion() and source then
+    return lib.notify(source, { description = message, type = type })
+  end
+
+  lib.notify({ description = message, type = type })
+end
+
 return config
