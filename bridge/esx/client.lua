fc596d88f68aeeaea0eccaeeafe776b2fe6a808e
juddlie <271890573+juddlie@users.noreply.github.com>
2026-03-30 21:37:48 -0400

    chore(bridge): remove unused files

--- bridge/esx/client.lua
+++
@@ -1,5 +0,0 @@
-if GetResourceState("es_extended") ~= "started" then error("es_extended is not started") end
-
-local esx = {}
-
-return esx
--- bridge/ox/client.lua
+++
@@ -1,5 +0,0 @@
-if GetResourceState("ox_core") ~= "started" then error("ox_core is not started") end
-
-local ox = {}
-
-return ox
